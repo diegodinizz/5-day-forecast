9d88e38dfa5d3ff39788e9c857028d2dfaa3d843
Diego Diniz <diegodinizz@gmail.com>
2020-08-24 11:17:33 +0100

    Updates

---
+++ precache-manifest.7d174c554f53ecf6f395bb82209050af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3114db4ce062613bcfa75e55fcb2ad7",
+    "url": "/5-day-forecast/index.html"
+  },
+  {
+    "revision": "0c164ca80065aa327afc",
+    "url": "/5-day-forecast/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eeb9f7bf658218422818",
+    "url": "/5-day-forecast/static/js/2.420133bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/5-day-forecast/static/js/2.420133bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c164ca80065aa327afc",
+    "url": "/5-day-forecast/static/js/main.ce8ca677.chunk.js"
+  },
+  {
+    "revision": "0b2ead4dc8a5169ca248",
+    "url": "/5-day-forecast/static/js/runtime-main.50cc0903.js"
+  }
+]);
